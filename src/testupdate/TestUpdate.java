b3f5234c25ba957f72025a74ef1551546af7896e
Javier <Javier@192.168.1.70>
2017-07-22 17:19:41 -0500

    test

--- src/testupdate/TestUpdate.java
+++ src/testupdate/TestUpdate.java
@@ -15,7 +15,7 @@ public class TestUpdate {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("reaizando modificaciones");
     }
     
 }
